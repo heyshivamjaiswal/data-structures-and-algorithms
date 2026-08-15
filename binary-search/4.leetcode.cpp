929f337a55762ce088a0b4ddfdcfaf98f544e776
heyshivamjaiswal <jaiswalsunny4656@gmail.com>
2026-08-15 21:13:19 +0530

    added leetcode 4

---
+++ binary-search/4.leetcode.cpp
@@ -0,0 +1,50 @@
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+       int n1 = nums1.size();
+       int n2 = nums2.size();
+
+       if(n1 > n2) return findMedianSortedArrays(nums2 , nums1);
+
+       int low = 0;
+       int high = n1;
+
+       // Use (n1 + n2 + 1) / 2 to make the left partition clear for odd totals
+       int left = (n1 + n2 + 1) / 2;
+
+       // Corrected to represent total combined size of both arrays
+       int n = n1 + n2; 
+
+       while(low <= high){
+        int mid1 = (low+high) >> 1;
+        int mid2 = left-mid1;
+
+        int l1 = INT_MIN;
+        int l2 = INT_MIN;
+
+        int r1 = INT_MAX;
+        int r2 = INT_MAX;
+
+        // CORRECTED INDEXING: mid1 belongs to nums1, mid2 belongs to nums2
+        if(mid1 < n1) r1 = nums1[mid1];
+        if(mid2 < n2) r2 = nums2[mid2];
+
+        // CORRECTED INDEXING: mid1-1 belongs to nums1, mid2-1 belongs to nums2
+        if(mid1-1 >= 0) l1 = nums1[mid1-1];
+        if(mid2-1 >= 0) l2 = nums2[mid2-1];
+
+        if(l1 <= r2 && l2 <= r1){
+            if(n % 2 == 1) {
+                return max(l1 , l2);
+            }
+            // CORRECTED: Fixed typo min(r1, 2) to min(r1, r2)
+            return (double)(max(l1,l2) + min(r1, r2)) / 2.0;
+        }
+        else if(l1 > r2) high = mid1-1;
+        else low = mid1+1;
+       }
+       return 0;
+    }
+};
