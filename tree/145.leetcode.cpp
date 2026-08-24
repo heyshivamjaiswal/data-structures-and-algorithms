b932f448681a5e78cc17b6e90a8144ce1aabceb4
Shivam Jaiswal <153024426+heyshivamjaiswal@users.noreply.github.com>
2026-08-24 15:03:12 +0530

    Implement postorder traversal for binary tree

---
+++ tree/145.leetcode.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   void helper(TreeNode*node , vector<int>&res){
+    if(node == nullptr) return;
+
+    helper(node->left , res);
+    helper(node->right , res);
+
+    res.push_back(node->val);
+   }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        helper(root , res);
+        return res;
+    }
+};
