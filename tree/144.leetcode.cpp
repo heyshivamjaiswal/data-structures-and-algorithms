6fa0759de82cc6d3fe25d310680c19ae3bccf90b
Shivam Jaiswal <153024426+heyshivamjaiswal@users.noreply.github.com>
2026-08-24 14:56:06 +0530

    Update 144.leetcode.cpp

--- tree/144.leetcode.cpp
+++ tree/144.leetcode.cpp
@@ -12,3 +12,19 @@ vector<int> preorderTraversal(TreeNode* root) {
 
     return res;
 }
+
+
+//Optimal 
+
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    helper(root, res);
+    return res;
+}
+
+void helper(TreeNode* node, vector<int>& res) {
+    if (node == nullptr) return;
+    res.push_back(node->val);
+    helper(node->left, res);
+    helper(node->right, res);
+}
