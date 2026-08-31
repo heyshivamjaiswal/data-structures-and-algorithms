6bdc1aa9e36baee7d4f1cd15b733496aaa15cd08
Shivam Jaiswal <153024426+heyshivamjaiswal@users.noreply.github.com>
2026-08-31 20:16:31 +0530

    Update 104.leetcode.cpp

--- tree/104.leetcode.cpp
+++ tree/104.leetcode.cpp
@@ -20,3 +20,31 @@ public:
         return 1 + max(lh,rh);
     }
 };
+
+
+//METHOD 2 
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+
+        while (!q.empty()) {
+            int size = q.size();  // number of nodes at current level
+            depth++;              // we are entering a new level
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+
+        return depth;
+    }
+};
