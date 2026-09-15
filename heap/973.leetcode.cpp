b37855e2b428e54ce7803eb6213be1f449dea273
Shivam Jaiswal <153024426+heyshivamjaiswal@users.noreply.github.com>
2026-09-15 21:53:59 +0530

    Create 973.leetcode.cpp

---
+++ heap/973.leetcode.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int , pair<int , int>>>pq;
+
+        for(auto & point: points){
+            int x = point[0];
+            int y = point[1];
+
+            int distance = x*x+y*y;
+
+            pq.push({distance , {x,y}});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+       vector<vector<int>> ans;
+
+        while(!pq.empty()){
+            auto point = pq.top().second;
+            ans.push_back({point.first, point.second});
+
+            pq.pop();
+        }
+        return ans;
+    }
+};
